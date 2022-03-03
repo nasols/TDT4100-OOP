9ce94213a4d0e91bf3f19cb96d5470c9a0a2d492
nasols <nasbrigtols@gmail.com>
2022-03-03 21:16:32 +0100

    1

--- src/main/java/fire/Test.java
+++ src/main/java/fire/Test.java
@@ -1,7 +1,12 @@
 package fire;
 
+
 public class Test {
+    static String kuk = "tissen min e lang og tjukk";
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(kuk);
+
+
     }
 }
