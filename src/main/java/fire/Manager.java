9927a695923f0f8ce17004e8eb0f74b58c9231e3
nasols <nasbrigtols@gmail.com>
2022-03-08 12:58:03 +0100

    1

--- src/main/java/fire/Manager.java
+++ src/main/java/fire/Manager.java
@@ -6,8 +6,11 @@ import java.util.List;
 public class Manager {
 
     User currentUser; 
+    String currentUsername; 
 
     List<User> users = new ArrayList<>();
+    List<String> userNames = new ArrayList<>();
+
     List<RentalPlace> rentalPlaces = new ArrayList<>();
 
     // sjekker om user allerede ligger i listen over brukere
@@ -26,19 +29,23 @@ public class Manager {
     public void login(String username){
         User newUser = new User(username);
 
-        if(validateUsername(newUser) == false){
+        if(validateUsername(newUser) == true){
             users.add(newUser);
+            userNames.add(username);
             this.currentUser = newUser;
+            this.currentUsername = username;
 
         }
         else{
             this.currentUser = newUser; 
+            this.currentUsername = username;
+
         }
 
     }
 
     public void newRentalPlace(String name, String description, CharSequence availableStart, CharSequence availableEnd, String ... args){
-        
+
         currentUser.newRentalPlace(name, description, availableStart, availableEnd, args);
         RentalPlace newRentalPlace = currentUser.rentalPlaces.get(currentUser.rentalPlaces.size() - 1);
 
@@ -50,5 +57,48 @@ public class Manager {
         }
 
     }
+
+    public void newRating(int score, String comment, Object UserOrPlace){
+        currentUser.newRating(score, comment, UserOrPlace);
+
+    }
+
+    public User getUser(String username){
+        int index = userNames.indexOf(username);
+
+        if(index != -1){
+            return users.get(index);
+
+        }
+        else{
+            throw new IllegalArgumentException("ingen bruker ved det brukernavnet");
+
+        }
+    
+        
+    }
+    public User getCurrentUser(){
+        int index = userNames.indexOf(currentUsername);
+
+        return users.get(index);
+
+
+    }
+
+
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+
+        manager.login("Jonas");
+        System.out.println(manager.users.get(0).username);
+        manager.login("Henrik");
+        System.out.println(manager.users.get(1).username);
+        manager.getCurrentUser().newRating(5, "kuk", manager.getUser("Jonas"));
+        manager.login("Jonas");
+        System.out.println(manager.getCurrentUser().personRatings.get(0).getComment());
+    
+    }
+
+
     
 }
